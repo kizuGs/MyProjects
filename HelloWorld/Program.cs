41f50f0f20ce72cbd566536f4e39090862201cd6
kizuG <swywijas@gmail.com>
2019-05-22 20:09:28 +0200

    test

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,21 +1,18 @@
+// A Hello World! program in C#.
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace HelloWorld
 {
-    class Program
+    class Hello
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
+
+
